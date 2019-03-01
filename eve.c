8a5367171c49063b225ca6d287320b33c0ccfb76
purushothaman1998 <48013992+purushothaman1998@users.noreply.github.com>
2019-03-01 05:28:15 -0800

    Create eve.c

---
+++ eve.c
@@ -0,0 +1,16 @@
+include <stdio.h>
+int main() 
+{
+    int m,n,sum=0;
+    scanf("%d %d",&m,&n);
+    sum=m+n;
+    if(sum%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+	return 0;
+}
